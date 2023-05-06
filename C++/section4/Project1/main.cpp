2a92a4ff07e235650ebe0bc85419273e8909e1de
diet-teacher <lsw4556@daum.net>
2023-05-06 17:39:50 +0900

    add c++

---
+++ C++/section4/Project1/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+ int main() {
+    std::cout << "Hello project1" << endl;
+    return 0;
+}
---
+++ C++/section4/Project2/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    std::cout << "Hello project2" << endl;
+    return 0;
+}
