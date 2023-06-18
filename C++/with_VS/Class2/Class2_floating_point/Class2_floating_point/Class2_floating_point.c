6dfef4041af292e4c1cc33a57b4beead2fda66aa
diet-teacher <lsw4556@daum.net>
2023-06-18 20:18:15 +0900

    C++ study

---
+++ C++/with_VS/Class2/Class2_floating_point/Class2_floating_point/Class2_floating_point.c
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+	123.0;		//double;
+	123.0F;		//float;
+
+	printf("%f\n", 123.45F);
+	printf("%f\n", 123.45);
+	printf("%f\n", 2147483648.0F);
+	printf("%f\n", 2147483648.0F - 20);
+	printf("%f\n", 2147483648.0F - 40);
+	printf("%f\n", 2147483648.0F - 60);
+	printf("%f\n", 2147483648.0F - 64);
+	printf("%f\n", 2147483648.0F - 65);
+	printf("%f\n", 247483.0F - 65);
+	printf("%f\n", 2474838.0F - 65);
+
+	printf("\nfloat type은 소수점 이하 6자리 \ndouble type은 소수점 이하 15자리 까지만 제대로 작동한다. \n부동 소수점 오차 때문\n");
+	return 0;
+}
---
+++ C++/with_VS/Class2/char/char/char.c
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+	/*char a = 'A';
+	printf("%c\n", a);
+	printf("%d\n", a);*/
+
+	//char szBuffer[12] = { 'H', 'e', 'l' };
+	char szBuffer[12] = {"Hel"};
+	puts(szBuffer);
+
+	return 0;
+}
